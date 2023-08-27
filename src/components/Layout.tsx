96d8d2b1383075f7f56bbd44a49c126a14a56abc
Ryan Reeves <reeves0ryan@gmail.com>
2023-08-27 14:41:56 -0700

    Import colors to tailwind, add navbar and layout components

---
+++ src/components/Layout.tsx
@@ -0,0 +1,23 @@
+import React, { type PropsWithChildren } from "react";
+import Navbar from "./Navbar";
+import { Toaster } from "react-hot-toast";
+
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <>
+      <Navbar />
+      <main className="flex min-h-[calc(100vh-66px)] flex-col items-center bg-gradient-to-l from-blue-800 to-indigo-900 p-4">
+        {children}
+      </main>
+      <Toaster
+        toastOptions={{
+          style: {
+            background: "rgb(30 41 59)",
+            border: "1px solid rgb(15 23 42 / 35%)",
+            color: "#f8fafc",
+          },
+        }}
+      />
+    </>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,21 @@ export default {
     fontFamily: {
       sans: ["FLF", "sans-serif"],
     },
+    colors: {
+      blue: {
+        light: "#9cd3be",
+        dark: "#7eac91",
+      },
+      green: {
+        light: "#c8e3b3",
+        olive: "#d7df22",
+      },
+      peach: {
+        light: "#fed8b8",
+      },
+      offblack: "#191919",
+      offwhite: "#f5f6f6",
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {},
