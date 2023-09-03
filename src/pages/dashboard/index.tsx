fc7aceaca142cb0a0097100ff1c6b517036ff3f8
Ryan Reeves <reeves0ryan@gmail.com>
2023-09-02 23:13:42 -1000

    Add head to dashboard and edit head for home

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { signOut } from "next-auth/react";
 import React, { useEffect } from "react";
 import useFetch from "../hooks/useFetch";
+import Head from "next/head";
 
 export default function Page() {
   const { fetchByType } = useFetch();
@@ -11,7 +12,7 @@ export default function Page() {
         if (res?.data.results[0]) {
           console.log(res);
         } else {
-          console.error(
+          throw new Error(
             `No results found for request: ${res?.request.responseURL}`
           );
         }
@@ -20,13 +21,20 @@ export default function Page() {
   }, []);
 
   return (
-    <div>
-      <button
-        className="bg-white/10 text-white hover:bg-white/20 rounded-full px-10 py-3 font-semibold no-underline transition"
-        onClick={() => void signOut({ redirect: true, callbackUrl: "/" })}
-      >
-        Sign Out
-      </button>
-    </div>
+    <>
+      <Head>
+        <title>Cupboard - Dashboard</title>
+        <meta name="description" content="Your dashboard" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <button
+          className="bg-white/10 text-white hover:bg-white/20 rounded-full px-10 py-3 font-semibold no-underline transition"
+          onClick={() => void signOut({ redirect: true, callbackUrl: "/" })}
+        >
+          Sign Out
+        </button>
+      </div>
+    </>
   );
 }
